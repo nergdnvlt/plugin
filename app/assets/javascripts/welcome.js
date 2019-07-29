3bc2f41595c2f0cad6b65fca583cca9425200ae2
Tyler Lundgren <tylerlundgren@yahoo.com>
2019-07-29 16:14:12 -0600

    Finished passing user id to api, need to index user id and also resolve JS promise.

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,3 +1,21 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://coffeescript.org/
+let userId = ""
+
+function buyProd(fsProdPath) {
+  fastspring.builder.add(fsProdPath);
+  fastspring.builder.checkout();
+}
+
+function popupWebhookReceived(fsData) {
+  if ( fsData.id ) {
+    userId = fsData.id
+    let user = { id: userId }
+    fetch('api/v1/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(user)
+    })
+    .then(function(response) { return response.json(); })
+  }
+}
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -4,10 +4,13 @@
 
 class Api::V1::UsersController < ApiController
   def create
-    user = User.find_or_create_by(email: params[:email])
-    user.first_name = params[:name].split[0] if user.first_name == nil
-    user.last_name = params[:name].split[1] if user.last_name == nil
-    user.save!
+    User.find_or_create_by(id: user_params[:id])
     render plain: "", status: 200
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:id)
   end
 end
