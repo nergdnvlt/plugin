258ad67f0545af8a078d0506069354ba00cb5aba
Tyler Lundgren <tylerlundgren@yahoo.com>
2019-08-07 09:51:20 -0600

    Finished webhook receiver.

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -7,10 +7,12 @@ class Api::V1::OrdersController < ApiController
   def create
     order_id = params[:events][0][:data][:order]
     order = Order.find_by(order_id: order_id)
-    if order.active == false
-      order.active = true
+    order.line_items.each do |line_item|
+      if line_item.active == false
+        line_item.active = true
+        line_item.save!
+      end
     end
-    order.save!
     render json: order, status: 200
   end
 end
